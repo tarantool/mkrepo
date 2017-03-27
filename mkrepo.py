f2548f7bd68f1b13d56a794f02d575e2ae7ac888
Nicola Asuni <nicola.asuni@miracl.com>
2017-03-27 09:40:45 +0100

    Change default temp repo from .tmp to .mkrepo

--- mkrepo.py
+++ mkrepo.py
@@ -60,7 +60,7 @@ def main():
 
     parser.add_argument(
         '--temp-dir',
-        default="./.tmp",
+        default="./.mkrepo",
         help='directory used to store temporary artifacts'
     )
 
