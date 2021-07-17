f5b5ff3ad2517f6e1ef955feaf8c5a50b0235f77
Leonid Vasiliev <lvasiliev@tarantool.org>
2021-07-17 16:36:57 +0300

    deb: implement parsing of package name according to specification
    Before the patch, it is assumed that the deb package name format is
    <package>_<version>.<revision>-<dist>_<arch>.deb.
    But when trying to use this formatt, we have several problems:
    https://github.com/knazarov/mkrepo/issues/16
    https://github.com/knazarov/mkrepo/issues/14
    
    So now we asume the deb package name format is
    <package-name>_<upstream-version>-<debian.revision>_<architecture>.deb
    according to https://www.debian.org/doc/manuals/debian-reference/ch02.en.html#_debian_package_file_names
    
    Closes #16

--- test/test_debrepo.py
+++ test/test_debrepo.py
@@ -17,25 +17,47 @@ class TestVersionParsing(unittest.TestCase):
                 'pool/deb9/main/v/vim/vim_8.2.2434-3_amd64.deb',
                 ('deb9', 'main', 'amd64')
             ),
-            # Commented out because it fails to parse properly at the moment
-            # The "dist" should be "main", but parses to "1" with current parser
-            #
-            # (
-            #     'pool/main/t/tarantool/tarantool-python_0.6.6-21_amd64.deb',
-            #     ('all', 'main', 'amd64')
-            # ),
-            #
-            # Neither does this
-            #
-            # (
-            #     'debian/pool/main/o/openssl/libssl1.1_1.1.0l-1~deb9u1_amd64.deb',
-            #     ('1~deb9u1', 'main', 'amd64')
-            # ),
+            (
+                'pool/main/t/tarantool/tarantool-python_0.6.6-21_amd64.deb',
+                ('all', 'main', 'amd64')
+            ),
+            (
+                'debian/pool/main/o/openssl/libssl1.1_1.1.0l-1~deb9u1_amd64.deb',
+                ('all', 'main', 'amd64')
+            ),
+            (
+                'pool/multiverse/a/anbox/anbox_0.0~git20200526-1build1_amd64.deb',
+                ('all', 'main', 'amd64')
+            ),
+            (
+                'pool/multiverse/a/astrometry-data-2mass/astrometry-data-2mass_1.1_all.deb',
+                ('all', 'main', 'all')
+            ),
         ]
 
         for version in versions:
             self.assertEqual(
-                debrepo.split_control_file_path(version[0], "binary"), version[1])
+                debrepo.split_control_file_path(version[0], 'binary'), version[1])
+
+    def test_versions_parsing_on_ubuntu_groovy(self):
+        """Check packagename parser on packages from
+        https://mirror.yandex.ru/ubuntu/dists/groovy
+        """
+        cur_dir = os.path.dirname(os.path.abspath(__file__))
+
+        test_file_names = [
+            'ubuntu_groovy_packages_main.txt',
+            'ubuntu_groovy_packages_multiverse.txt',
+            'ubuntu_groovy_packages_restricted.txt',
+            'ubuntu_groovy_packages_universe.txt'
+        ]
+
+        for test_file_name in test_file_names:
+            test_path = os.path.join(cur_dir, test_file_name)
+            with open(test_path, 'r') as test_file:
+                for package_name in test_file:
+                    self.assertIsNotNone(debrepo.split_control_file_path(package_name, 'binary'),
+                                         "Can't parse packagename: %s" % package_name)
 
 
 if __name__ == '__main__':
