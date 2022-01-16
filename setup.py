cdc6ef352476c2ed7ec18cb38583328deaf1baf5
Konstantin Nazarov <mail@knazarov.com>
2022-01-16 22:50:47 +0000

    Bump package version to 0.1.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name='mkrepo',
     packages=[''],
-    version='0.1.4',
+    version='0.1.5',
     description='Maintain deb and rpm repos on s3',
     author='Konstantin Nazarov',
     author_email='mail@kn.am',
