dede2c7c7f68cba393a174770ab1eca9894dd4d3
Konstantin Nazarov <mail@knazarov.com>
2022-06-03 15:48:01 +0100

    Bump version to 0.1.6

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
     name='mkrepo',
     packages=[''],
-    version='0.1.5',
+    version='0.1.6',
     description='Maintain deb and rpm repos on s3',
     long_description=long_description,
     long_description_content_type="text/markdown",
