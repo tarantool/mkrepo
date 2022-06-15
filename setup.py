6e0b3d17a956681d09f7b845ec7397c1d8ee44ae
Yaroslav Lobankov <y.lobankov@tarantool.org>
2022-06-15 12:43:06 +0300

    test: fix imports order to pass isort checks
    This patch fixes the following isort error:
    
        ERROR: /home/runner/work/mkrepo/mkrepo/setup.py Imports are
        incorrectly sorted and/or formatted.

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
-from setuptools import setup
 from os import path
+
+from setuptools import setup
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
