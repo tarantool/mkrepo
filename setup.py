d4f05e733d33926418e39ccc021c54474880340e
Yaroslav Lobankov <y.lobankov@tarantool.org>
2022-11-23 18:22:37 +0700

    Release 1.0.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
     name='mkrepo',
     packages=[''],
-    version='0.1.10',
+    version='1.0.0',
     description='Maintain deb and rpm repos on s3',
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -19,6 +19,21 @@ setup(
     author_email='mail@kn.am',
     url='https://github.com/tarantool/mkrepo',
     keywords=['rpm', 'deb'],
-    classifiers=[],
+    python_requires='>=3.6.*',
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: System :: Software Distribution',
+        'Topic :: System :: Systems Administration',
+    ],
     scripts=['mkrepo']
 )
